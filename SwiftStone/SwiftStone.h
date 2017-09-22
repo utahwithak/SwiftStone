e071bb119cafd43e72740e90ddae1fc0936d7cc6
Carl Wieland <cwieland@esri.com>
2017-09-21 20:33:21 -0400

    initial framework

---
+++ SwiftStone/SwiftStone.h
@@ -0,0 +1,19 @@
+//
+//  SwiftStone.h
+//  SwiftStone
+//
+//  Created by Carl Wieland on 9/21/17.
+//  Copyright © 2017 Datum Apps. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for SwiftStone.
+FOUNDATION_EXPORT double SwiftStoneVersionNumber;
+
+//! Project version string for SwiftStone.
+FOUNDATION_EXPORT const unsigned char SwiftStoneVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftStone/PublicHeader.h>
+
+
